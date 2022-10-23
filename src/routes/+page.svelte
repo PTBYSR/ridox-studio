37aab6171e72ab50606567f332071edf58c95461
PTBYSR <paul.emechebe@gmail.com>
2022-10-23 02:54:45 +0100

    seo fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	import Landing from '../components/sections/landing.svelte';
 	import About from '../components/sections/about.svelte';
 	import Footer from '../components/sections/footer.svelte';
+    import { MetaTags } from 'svelte-meta-tags';
 </script>
 
 <svelte:head>
@@ -14,6 +15,44 @@
 		rel="stylesheet"
 	/>
 </svelte:head>
+<MetaTags
+  title="Ridox Studio"
+  titleTemplate="%s | "
+  description="Get a website connected to your Whatsapp Business on a budget"
+  canonical=""
+  openGraph={{
+    url: 'https://ridox-studio.vercel.app/',
+    title: 'Ridox Studio',
+    description: 'Open Graph Description',
+    images: [
+      {
+        url: 'https://ridox-studio.vercel.app/ridox-header-logo.png',
+        width: 800,
+        height: 600,
+        alt: ''
+      },
+      {
+        url: 'https://ridox-studio.vercel.app/ridox-header-logo.png',
+        width: 900,
+        height: 800,
+        alt: 'Og Image Alt Second'
+      },
+    ],
+    site_name: 'Ridox Studio'
+  }}
+  twitter={{
+    handle: '@ptbthefirst',
+    site: '@site',
+    cardType: 'summary',
+    title: 'Ridox Studio',
+    description: 'Get a website connected to your Whatsapp Business on a budget.',
+    image: 'https://ridox-studio.vercel.app/ridox-header-logo.png',
+    imageAlt: 'Twitter image alt'
+  }}
+  facebook={{
+    appId: '1234567890'
+  }}
+/>
 <Header />
 <Landing />
 <Pricing />
