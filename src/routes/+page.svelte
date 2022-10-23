780d0e89c4fe35a14b34c4c1133de8d773eab8fc
PTBYSR <paul.emechebe@gmail.com>
2022-10-23 02:30:06 +0100

    font download

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,19 @@
 <script>
-    import Pricing from '../components/sections/pricing.svelte';
-    import Header from '../components/sections/header.svelte'
-    import Landing from '../components/sections/landing.svelte';
+	import Pricing from '../components/sections/pricing.svelte';
+	import Header from '../components/sections/header.svelte';
+	import Landing from '../components/sections/landing.svelte';
 	import About from '../components/sections/about.svelte';
 	import Footer from '../components/sections/footer.svelte';
 </script>
 
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Inter&family=Libre+Baskerville&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
 <Header />
 <Landing />
 <Pricing />
